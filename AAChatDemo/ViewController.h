6a185f04eeb07777bffd7d0a95019e41f105b183
congcongliu <allium@zhuzhu56.com>
2018-01-09 20:53:12 +0800

    first commit

---
+++ AAChatDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AAChatDemo
+//
+//  Created by CongCong on 2018/1/9.
+//  Copyright © 2018年 CongCong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AAChatDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  AAChatDemo
+//
+//  Created by CongCong on 2018/1/9.
+//  Copyright © 2018年 CongCong. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "AAChartView.h"
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
